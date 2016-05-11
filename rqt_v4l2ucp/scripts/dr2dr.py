9795799c202ba2313e947914bc5b6b7558e4c401
Lucas Walter <wsacul@gmail.com>
2016-05-10 20:34:22 -0700

    Upstream to local works, but not other way around

--- rqt_v4l2ucp/scripts/dr2dr.py
+++ rqt_v4l2ucp/scripts/dr2dr.py
@@ -16,6 +16,7 @@
 # TODO support enum and hierarchy late later
 
 import copy
+import dynamic_reconfigure
 import rospy
 
 # from dynamic_reconfigure.parameter_generator_catkin import *
@@ -37,7 +38,8 @@ class Dr2Dr():
 
     def config(self, msg):
         self.values = {}
-        self.server = {}
+        self.client = {}
+        self.client_of_param = {}
         self.server_params = {}
         self.server_value_name = {}
 
@@ -46,18 +48,21 @@ class Dr2Dr():
         #rospy.loginfo(rospy.get_namespace())
         all_params = rospy.get_param_names()
         prefix = rospy.get_namespace() + "controls/"
-        print rospy.get_namespace()
+        rospy.logdebug(rospy.get_namespace())
         for param in all_params:
             if param[0:len(prefix)] == prefix:
 
                 config = rospy.get_param(param)
                 param = param.replace(prefix, "")
-                rospy.loginfo(param)
+                rospy.logdebug(param)
                 # rospy.loginfo(config)
                 #self.server[param] = config[0]
-                if not config[0] in self.server_params.keys():
-                    self.server_params[config[0]] = []
-                self.server_params[config[0]].append(param)
+                server_name = config[0]
+                if not server_name in self.server_params.keys():
+                    self.server_params[server_name] = []
+                self.server_params[server_name].append(param)
+                rospy.loginfo("new param: " + server_name + " " + param + " " +
+                              str(self.server_params[server_name]))
 
                 self.server_value_name[param] = config[1]
                 base_cfg.type[param] = config[2]
@@ -78,33 +83,73 @@ class Dr2Dr():
                 parameter['max'] = base_cfg.max[param]
                 parameter['level'] = base_cfg.level[param]
                 base_cfg.config_description['parameters'].append(parameter)
-
-        for key in self.server_params.keys():
-            # TODO(lwalter) if the server doesn't exist yet, retry later
-            self.server[key] = Client(key, timeout=1,
-                                      config_callback=partial(self.upstream_dr_callback,
-                                                              self.server_params[key]))
         # TODO(lucasw) if no params are found raise a warning
+
+        self.configured = False
+        self.break_feedback = False
+        self.break_feedback2 = False
 
         self.base_cfg = base_cfg
         self.dr_server = Server(base_cfg, self.dr_callback)
 
-    # the callback from this one server 
+        rospy.loginfo(self.server_params)
+        for server_name in self.server_params.keys():
+            params = self.server_params[server_name]
+            # rospy.loginfo(server_name + " " + str(params))
+            self.client[server_name] = None
+            try:
+                self.client[server_name] = Client(server_name, timeout=1,
+                                                  config_callback=partial(self.upstream_dr_callback,
+                                                                          params))
+            except rospy.exceptions.ROSException as e:
+                # TODO(lucasw) if the server doesn't exist yet, retry later
+                rospy.logwarn(e)
+
+            for param in params:
+                rospy.loginfo(param + " " + server_name)
+                self.client_of_param[param] = self.client[server_name]
+        self.configured = True
+
+    # the callback from this server update the other servers through the clients
     def dr_callback(self, config, level):
+        if not self.configured:
+            return config
+        if self.break_feedback:
+            return config
         for key in config.groups.parameters.keys():
             if level & self.base_cfg.level[key]:
-                rospy.loginfo(str(level) + " " + key)
-                # self.pubs[key].publish(Int32(config[key]))
+                server_key_name = self.server_value_name[key]
+                value = config[key]
+                # TODO(lucasw) conglomerate updates from the same server
+                if self.client_of_param[key]:
+                    self.break_feedback2 = True
+                    try:
+                        self.client_of_param[key].update_configuration({server_key_name: value})
+                    except dynamic_reconfigure.DynamicReconfigureParameterException as e:
+                        rospy.loginfo(str(level) + " " + key + ": " + str(value))
+                        rospy.logwarn(e)
+                    self.break_feedback2 = False
+                # TODO(lucasw) else try creating the client again- or 
+                # have timered update that tries that at regular rate?
         return config
 
-    # the callback from the all the other upstream Servers
-    def upstream_dr_callback(self, config, params):
-        rospy.loginfo(config)
-        rospy.loginfo(params)
-        # Update the local values
+    # the callback from the all the upstream servers updates the local server
+    # TODO(lucasw) thought config would come before params from partial,
+    # but that is not the case.
+    def upstream_dr_callback(self, params, config):
+        if self.break_feedback2:
+            return
+        # rospy.loginfo('config: ' + str(config))
+        # rospy.loginfo('params: ' + str(params))
+        updates = {}
+        for param in params:
+            updates[param] = config[self.server_value_name[param]]
+        # rospy.loginfo(updates)
+        # Update the local values, which will in turn trigger dr_callback
+        self.break_feedback = True
+        self.dr_server.update_configuration(updates)
+        self.break_feedback = False
         # http://wiki.ros.org/dynamic_reconfigure/Tutorials/UsingTheDynamicReconfigurePythonClient
-        # look through config names and see which ones match up to params
-        # self.server[key].update_configuration({params[0]: config, params[1]: ... })
 
 if __name__ == "__main__":
     rospy.init_node("dr2dr")
