a9b308107c98f526a211f3ef9bd329c4648bd913
Lucas Walter <wsacul@gmail.com>
2018-02-27 19:17:10 -0800

    Simulate a locked up callback with a long sleep #15

--- rqt_dr_single/scripts/example_server.py
+++ rqt_dr_single/scripts/example_server.py
@@ -5,13 +5,25 @@ import rospy
 from dynamic_reconfigure.server import Server
 from rqt_dr_single.cfg import ExampleConfig
 
+count = 0
+hangup = False
+
 def callback(config, level):
+    global count
+    global hangup
     rospy.loginfo("""Reconfigure Request: {int_param}, {double_param},\ 
           {str_param}, {bool_param}, {enum_param}""".format(**config))
+
+    if hangup:
+        count += 1
+        if count > 2:
+            rospy.loginfo("going to sleep for a while")
+            rospy.sleep(10.0)
     return config
 
 if __name__ == "__main__":
     rospy.init_node("rqt_dr_single", anonymous = True)
 
+    hangup = rospy.get_param("~hangup", False)
     srv = Server(ExampleConfig, callback)
     rospy.spin()
