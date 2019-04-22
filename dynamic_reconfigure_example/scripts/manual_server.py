e8032cac7558ae5fec2a3f3d484dcc4041f71f5e
Lucas Walter <wsacul@gmail.com>
2019-04-22 16:53:51 -0700

    Refactored into class

--- dynamic_reconfigure_example/scripts/manual_server.py
+++ dynamic_reconfigure_example/scripts/manual_server.py
@@ -7,49 +7,62 @@ from dynamic_reconfigure.srv import *
 from dynamic_reconfigure.msg import *
 # from dynamic_reconfigure_example.cfg import ExampleConfig
 
-count = 0
-hangup = False
-die = False
+class ManualDr:
+    def __init__(self):
+        rospy.init_node('example_server_manual', anonymous = True)
 
-def handle_reconfigure(req):
-    rospy.loginfo(req)
-    resp = ReconfigureResponse()
-    return resp
+        ns = rospy.get_namespace() + '/' + rospy.get_name()
+        ns = ns.replace('///','/')
 
-def update(msg):
-    rospy.loginfo(msg)
+        rospy.loginfo('namespace: ' + ns)
+        self.pub = rospy.Publisher(ns + '/parameter_descriptions', ConfigDescription, queue_size=1, latch=True)
+        self.cd = ConfigDescription()
+        for name in ['test_bool', 'test_bool2']:
+            bool_param = BoolParameter()
+            bool_param.name = name
+            bool_param.value = True
+            self.cd.dflt.bools.append(bool_param)
+            self.cd.max.bools.append(bool_param)
+            bool_param.value = False
+            self.cd.min.bools.append(bool_param)
+
+        group = Group()
+        group.name = 'Default'
+        group.parent = 0
+        group.id = 0
+        param = ParamDescription()
+        param.name = bool_param.name
+        param.level = 1
+        param.type = 'bool'
+        param.description = 'test bool'
+        group.parameters.append(param)
+        self.cd.groups.append(group)
+
+        group = Group()
+        group.name = 'Test'
+        group.parent = 0
+        group.id = 1
+        param = ParamDescription()
+        param.name = bool_param.name
+        param.level = 1
+        param.type = 'bool'
+        param.description = 'test bool2'
+        group.parameters.append(param)
+        self.cd.groups.append(group)
+
+        self.pub.publish(self.cd)
+
+        self.sub = rospy.Subscriber(ns + '/parameter_updates', Config, self.update, queue_size=1)
+        self.srv = rospy.Service(ns + '/set_parameters', Reconfigure, self.handle_reconfigure)
+
+    def handle_reconfigure(self, req):
+        rospy.loginfo(req)
+        resp = ReconfigureResponse()
+        return resp
+
+    def update(msg):
+        rospy.loginfo(msg)
 
 if __name__ == "__main__":
-    rospy.init_node('example_server_manual', anonymous = True)
-
-    ns = rospy.get_namespace() + '/' + rospy.get_name()
-    ns = ns.replace('///','/')
-
-    rospy.loginfo('namespace: ' + ns)
-    pub = rospy.Publisher(ns + '/parameter_descriptions', ConfigDescription, queue_size=1, latch=True)
-    cd = ConfigDescription()
-    bool_param = BoolParameter()
-    bool_param.name = 'test_bool'
-    bool_param.value = True
-    cd.dflt.bools.append(bool_param)
-    cd.max.bools.append(bool_param)
-    bool_param.value = False
-    cd.min.bools.append(bool_param)
-
-    group = Group()
-    group.name = 'Default'
-    param = ParamDescription()
-    param.name = bool_param.name
-    param.level = 1
-    param.type = 'bool'
-    param.description = 'test bool'
-    group.parameters.append(param)
-    cd.groups.append(group)
-
-    pub.publish(cd)
-
-    sub = rospy.Subscriber(ns + '/parameter_updates', Config, update, queue_size=1)
-
-    srv = rospy.Service(ns + '/set_parameters', Reconfigure, handle_reconfigure)
-
+    manual_dr = ManualDr()
     rospy.spin()
