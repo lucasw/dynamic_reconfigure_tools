d35999de7304517883d441b5d9655b9187582e81
Lucas Walter <wsacul@gmail.com>
2021-09-29 16:55:42 -0700

    also support bool type

--- dynamic_reconfigure_tools/scripts/ddr_topic_pub.py
+++ dynamic_reconfigure_tools/scripts/ddr_topic_pub.py
@@ -14,8 +14,12 @@ class DDRTopics():
 
         self.msg_name = rospy.get_param("~msg_type", "std_msgs/Float64")
         self.msg_class = roslib.message.get_message_class(self.msg_name)
+        rospy.loginfo("{} -> {}".format(self.msg_name, self.msg_class))
+        if self.msg_class is None:
+            raise Exception("could not load message type {}".format(self.msg_name))
 
-        # TODO(lucasw) no per-topic min maxes, just these for all of them
+        # TODO(lucasw) no per-topic min/max/defaults, just these for all of them
+        default_value = rospy.get_param("~default", 0.0)
         min_value = rospy.get_param("~min", -1.0)
         max_value = rospy.get_param("~max", 1.0)
         max_len = rospy.get_param("~max_len", 30)
@@ -25,11 +29,14 @@ class DDRTopics():
         self.pubs = {}
         topics = rospy.get_param("~topics", ["out"])
         for topic in topics:
+            # TODO(lucasw) this isn't ideal but need to pare down the names
             name = topic.replace("/", "_").lstrip("_")[-max_len:]
             rospy.loginfo("{} -> {}".format(topic, name))
             self.pubs[name] = rospy.Publisher(topic, self.msg_class, queue_size=3)
-            # TODO(lucasw) this isn't ideal but need to pair down the names
-            self.ddr.add_variable(name, topic, 0.0, min_value, max_value)
+            if type(default_value) == bool:
+                self.ddr.add_variable(name, topic, default_value)
+            else:
+                self.ddr.add_variable(name, topic, default_value, min_value, max_value)
 
         self.ddr.start(self.dr_callback)
 
